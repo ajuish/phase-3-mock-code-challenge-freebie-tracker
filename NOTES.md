03f596c7161ca7162e7c61e10e0b4806671de591
Alex Ju <aju217@gmail.com>
2022-11-07 17:29:40 -0500

    commit tree 49098e5b755c2f3023d7bbf692ddc6e812defa06

---
+++ NOTES.md
@@ -0,0 +1,1 @@
+Company -< Freebie  >- Devs
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+puts "Destroying companies..."
+Company.destroy_all
+puts "Destroying devs..."
+Dev.destroy_all
+puts "Destroying freebies..."
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -11,7 +18,15 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+# id: nil, item_name: nil, value: nil, company_id: nil, dev_id: nil
+50.times do
+    Freebie.create(
+        company_id: Company.all.ids.sample,
+        dev_id: Dev.all.ids.sample,
+        item_name: Faker::Commerce.product_name,
+        value: rand(1..100)r
+    )
+end
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
